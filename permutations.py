f224a6e7bf3f9b970d608303fcb24d97cca5ba05
mona <jalal@wisc.edu>
2016-05-31 17:29:05 -0500

    various permutations

--- permutations.py
+++ permutations.py
@@ -2,3 +2,14 @@ from itertools import permutations
 
 for perm in permutations([1, 2, 3]):
     print(perm, end=" ")
+
+print()
+
+
+for perm in permutations("nice"):
+    print("".join(perm), end=" ")
+
+print()
+
+for perm in permutations("mona"):
+    print(perm, end=" ")
