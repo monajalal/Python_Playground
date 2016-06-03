52baccf6cec604ad9fe092311a4cbf0b0f5aa419
mona <jalal@wisc.edu>
2016-06-02 23:56:35 -0500

    regex

--- regex.py
+++ regex.py
@@ -9,3 +9,16 @@ print(re.match(r'Vader', names))
 print(re.search(r'Vader', names))
 print(re.match(r'\w+, \w+', names))
 print(re.search(r'\w+, \w+', names)) ###why?
+#why do I only get <_sre.SRE_Match object; span=(658, 670), match='555 555-5551'>
+print(re.search(r'(\d{3}) \d{3}-\d{4}', names))
+#why do I only get <_sre.SRE_Match object; span=(658, 669), match='555 555-555'>
+print(re.search(r'(\d\d\d) \d\d\d-\d\d\d', names))
+#got <_sre.SRE_Match object; span=(44, 58), match='(555) 555-5555'>
+print(re.search(r'\(\d{3}\) \d{3}-\d{4}', names))
+print(re.search(r'\(\d*\)',names))
+print(re.search(r'\w+, \w+', names))
+print(re.search(r'\(?\d*\)? \d{3}-\d{4}',names))
+print(re.findall(r'\(?\d*\)? \d{3}-\d{4}',names))
+
+
+
